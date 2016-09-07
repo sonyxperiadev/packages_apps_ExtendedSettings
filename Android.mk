b1d540f869bb66341a5ed1a016c69043ad41e998
Christian Oder <myself5@carbonrom.org>
2016-09-07 18:39:41 +0200

    Extended Settings: Initial Commit

---
+++ Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-design android-support-v7-appcompat
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+        $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+		      $(LOCAL_PATH)/../../../$(SUPPORT_LIBRARY_ROOT)/v7/appcompat/res \
+		      $(LOCAL_PATH)/../../../$(SUPPORT_LIBRARY_ROOT)/design/res
+
+LOCAL_PACKAGE_NAME := ExtendedSettings
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.design \
+    --extra-packages android.support.v7.appcompat
+
+include $(BUILD_PACKAGE)
+
+# Use the following include to make our test apk.
+ifeq (,$(ONE_SHOT_MAKEFILE))
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
