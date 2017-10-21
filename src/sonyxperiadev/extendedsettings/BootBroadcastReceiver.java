7d9eabddc48dd918e70981a93715b64ff416519a
Angelo G. Del Regno <kholk11@gmail.com>
2017-10-21 18:37:15 +0200

    DispCal: Detect device encryption status and delay auto setting
    If the device is encrypted, we cannot read the required
    property to get the current DispCal profile and we cannot
    write to sysfs for some reason.
    Detect device encryption status and, in case the device is
    encrypted, delay setting the display calibration to when
    the user has unlocked the device.

--- src/sonyxperiadev/extendedsettings/BootBroadcastReceiver.java
+++ src/sonyxperiadev/extendedsettings/BootBroadcastReceiver.java
@@ -1,10 +1,15 @@
 package sonyxperiadev.extendedsettings;
 
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserManager;
 import android.util.Log;
 
+import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_ACTIVATING;
+import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_INACTIVE;
+import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_UNSUPPORTED;
 /**
  * This will be executed after the core system has been initialized,
  * along with providers. This means that any dependency on any system
@@ -20,6 +25,25 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         try {
+            DevicePolicyManager polMan = (DevicePolicyManager)
+                    context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+            UserManager userMan = UserManager.get(context);
+            int encryptionStatus = polMan.getStorageEncryptionStatus();
+
+            /* If the device is being encrypted, do NOT touch ANYTHING */
+            if (encryptionStatus == ENCRYPTION_STATUS_ACTIVATING)
+                return;
+
+            /*
+             * If we are at a relatively early Android boot stage, check if the
+             * device is encrypted. If it is, bail out, as we cannot read system
+             * properties right now (and we cannot write to sysfs).
+             */
+            if (!userMan.isUserUnlocked() &&
+                    encryptionStatus != ENCRYPTION_STATUS_INACTIVE &&
+                    encryptionStatus != ENCRYPTION_STATUS_UNSUPPORTED)
+                return;
+
             String sysPref = ExtendedSettingsActivity.getSystemProperty(
                     ExtendedSettingsActivity.PREF_DISPCAL_SETTING);
             if (sysPref == null || sysPref.length() < 1)
