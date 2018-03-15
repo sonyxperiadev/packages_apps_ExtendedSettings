e62f19b33450f4593b5131919714c2589233b9b6
LuK1337 <priv.luk@gmail.com>
2018-03-15 17:40:06 +0100

    Reformat Android.mk

--- Android.mk
+++ Android.mk
@@ -1,20 +1,23 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-design android-support-v7-appcompat
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := \
-        $(call all-java-files-under, src)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-		      $(LOCAL_PATH)/../../../$(SUPPORT_LIBRARY_ROOT)/v7/appcompat/res \
-		      $(LOCAL_PATH)/../../../$(SUPPORT_LIBRARY_ROOT)/design/res
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := ExtendedSettings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
+    android-support-design \
+    android-support-v7-appcompat
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    frameworks/support/design/res \
+    frameworks/support/v7/appcompat/res
 
 LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
