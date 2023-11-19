90cd99ab2cf3343ee6059ebabcc0731d4713d5c6
Marijn Suijten <marijn.suijten@somainline.org>
2023-11-19 23:30:40 +0100

    Delete stale DRS / DispCal setup
    The code for Dynamic Resolution Setting / Display Calibration is stale,
    no longer supported / working and would need to be completely refactored
    to work on modern kernels _and compile with modern versions of Android_
    by properly talking to HWC2.
    
    Remove it for now until someone finds the time to make it work properly
    again, by reverting this commit and investigating the necessary sysfs /
    HWC2 toggles to poke.

--- src/sonyxperiadev/extendedsettings/BootBroadcastReceiver.java
+++ src/sonyxperiadev/extendedsettings/BootBroadcastReceiver.java
@@ -1,18 +1,18 @@
 package sonyxperiadev.extendedsettings;
 
+import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_ACTIVATING;
+import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_INACTIVE;
+import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_UNSUPPORTED;
+
 import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.UserManager;
-import android.os.SystemProperties;
 import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 
-import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_ACTIVATING;
-import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_INACTIVE;
-import static android.app.admin.DevicePolicyManager.ENCRYPTION_STATUS_UNSUPPORTED;
 /**
  * This will be executed after the core system has been initialized,
  * along with providers. This means that any dependency on any system
@@ -34,8 +34,9 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
             int encryptionStatus = polMan.getStorageEncryptionStatus();
 
             /* If the device is being encrypted, do NOT touch ANYTHING */
-            if (encryptionStatus == ENCRYPTION_STATUS_ACTIVATING)
+            if (encryptionStatus == ENCRYPTION_STATUS_ACTIVATING) {
                 return;
+            }
 
             /*
              * If we are at a relatively early Android boot stage, check if the
@@ -44,20 +45,20 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
              */
             if (!userMan.isUserUnlocked() &&
                     encryptionStatus != ENCRYPTION_STATUS_INACTIVE &&
-                    encryptionStatus != ENCRYPTION_STATUS_UNSUPPORTED)
+                    encryptionStatus != ENCRYPTION_STATUS_UNSUPPORTED) {
                 return;
+            }
 
             final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
-            final String dispCal = prefs.getString(ExtendedSettingsFragment.mDispCalSwitchPref, "0");
-
-            ExtendedSettingsFragment.performDisplayCalibration(Integer.parseInt(dispCal));
             ExtendedSettingsFragment
-                    .performGloveMode(prefs.getBoolean(ExtendedSettingsFragment.mGloveModeSwitchPref, false));
+                    .performGloveMode(
+                            prefs.getBoolean(ExtendedSettingsFragment.mGloveModeSwitchPref, false));
         } catch (Throwable t) {
             Log.wtf(TAG, "We have crashed. THIS IS AN HORRENDOUS BUG!");
-            Log.wtf(TAG, "Please report this error immediately by opening a new issue on GitHub.\n" +
-                    "--- https://git.io/vduAF --- Thank you!", t);
+            Log.wtf(TAG,
+                    "Please report this error immediately by opening a new issue on GitHub.\n" +
+                            "--- https://git.io/vduAF --- Thank you!", t);
         }
     }
 }
